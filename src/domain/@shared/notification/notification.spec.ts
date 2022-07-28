c3ca34c89f007c3b92d1daaeed0da7b6c4141304
debora-cristinaf <programacaodcrisf@gmail.com>
2022-07-27 22:29:06 -0300

    feat: added notification

---
+++ src/domain/@shared/notification/notification.spec.ts
@@ -0,0 +1,39 @@
+import Notification from "./Notification";
+
+describe("Unit test for notifications", () => {
+  it("should create errors", async () => {
+    const notification = new Notification();
+    const error = {
+      message: "error message",
+      context: "customer",
+    };
+
+    notification.addError(error);
+    expect(notification.messages("customer")).toBe("customer: error message,");
+
+    const error2 = {
+      message: "error message2",
+      context: "customer",
+    };
+
+    notification.addError(error2);
+    expect(notification.messages("customer")).toBe(
+      "customer: error message,customer: error message2,"
+    );
+
+    const error3 = {
+      message: "error message3",
+      context: "order",
+    };
+
+    notification.addError(error3);
+
+    expect(notification.messages("customer")).toBe(
+      "customer: error message,customer: error message2,"
+    );
+
+    expect(notification.messages()).toBe(
+      "customer: error message,customer: error message2,order: error message3,"
+    );
+  });
+});
