eae2b6904cd68e77527c3fd7b5a1ee4dd8525740
debora-cristinaf <programacaodcrisf@gmail.com>
2022-07-31 16:33:56 -0300

    feat: added yup validator

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import CustomerValidatorFactory from "../factory/customer.factory.validator";
 import Address from "../value-object/address";
 
 export default class Customer extends Entity {
@@ -28,18 +29,7 @@ export default class Customer extends Entity {
   }
 
   validate() {
-    if (this._id.length === 0) {
-      this.notification.addError({
-        context: "Customer",
-        message: "Id is required",
-      });
-    }
-    if (this._name.length === 0) {
-      this.notification.addError({
-        context: "Customer",
-        message: "Name is required",
-      });
-    }
+    CustomerValidatorFactory.create().validate(this);
   }
 
   changeName(name: string) {
