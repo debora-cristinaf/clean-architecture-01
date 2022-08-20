7929535d61b2ae6647f8fb2bf73c8778265e2c2b
debora-cristinaf <programacaodcrisf@gmail.com>
2022-08-20 09:40:21 -0300

    test: added test product validator

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -30,4 +30,12 @@ describe("Product unit tests", () => {
     product.changePrice(150);
     expect(product.price).toBe(150);
   });
+
+  it("should throw error when price is less than zero and name is empty", () => {
+    expect(() => {
+      const product = new Product("123", "", -1);
+    }).toThrowError(
+      "Product: Name is required,Product: price must be greater than 0"
+    );
+  });
 });
